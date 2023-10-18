7fd37da738e7fab04d57a1006d200f8538ee4916
LBlanka99 <losonci.blanka@mensa.hu>
2023-10-18 14:34:57 +0200

    Create markers on click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,9 @@
 import "./Map.css";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import mapboxgl from "mapbox-gl";
 
 const Map = () => {
+    const [markers, setMarkers] = useState([]);
 
     useEffect(() => {
         mapboxgl.accessToken = "pk.eyJ1IjoiYmxhbmthOTkiLCJhIjoiY2xudmZ4ZHI3MHBtajJrb2p4cGJuN2FkZiJ9.F8-MFO-f6jbjFS1zhItYgA";
@@ -14,16 +15,30 @@ const Map = () => {
             zoom: 12.5,
         });
 
-        map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector('body')}));
+        map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector("body")}));
         map.addControl(new mapboxgl.GeolocateControl());
+
+
+            const createMarker = (coordinates, mapInstance) => {
+                //TODO check if there is already a marker at these coordinates
+                const newMarker = new mapboxgl.Marker().setLngLat(coordinates).addTo(mapInstance);
+                setMarkers([...markers, newMarker]);
+            };
+
+            const handleMapClick = (e) => {
+                const lng = e.lngLat.lng;
+                const lat = e.lngLat.lat;
+                createMarker([lng, lat], map);
+            }
+
+            map.on("click", handleMapClick);
+
 
         return () => map.remove();
     }, []);
 
     return (
-        <div id={"map"}>
-
-        </div>
+        <div id={"map"}></div>
     );
 };
 

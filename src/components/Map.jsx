3d398567bb3f411bdf2640308d6174c4f94f880c
LBlanka99 <losonci.blanka@mensa.hu>
2023-10-18 20:20:24 +0200

    Place markers on the map by address search

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,7 @@
 import "./Map.css";
 import {useEffect, useState} from "react";
 import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 const Map = () => {
     const [markers, setMarkers] = useState([]);
@@ -15,30 +16,40 @@ const Map = () => {
             zoom: 12.5,
         });
 
-        map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector("body")}));
-        map.addControl(new mapboxgl.GeolocateControl());
+        const geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            marker: {
+                color: "red"
+            },
+            mapboxgl: mapboxgl,
+        });
+
+        map.addControl(geocoder, "top-right");
+
+        map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector("body")}), "bottom-right");
+        map.addControl(new mapboxgl.GeolocateControl(), "bottom-right");
 
 
-            const createMarker = (coordinates, mapInstance) => {
-                //TODO check if there is already a marker at these coordinates
-                const rotationDegree = Math.random() < 0.5 ? 10 : -10;
-                const newMarker = new mapboxgl.Marker({
-                    draggable: true,
-                    color: "orange",
-                    rotation: rotationDegree
-                }).setLngLat(coordinates)
-                    .addTo(mapInstance);
+        const createMarker = (coordinates, mapInstance) => {
+            //TODO check if there is already a marker at these coordinates
+            const rotationDegree = Math.random() < 0.5 ? 10 : -10;
+            const newMarker = new mapboxgl.Marker({
+                draggable: true,
+                color: "orange",
+                rotation: rotationDegree
+            }).setLngLat(coordinates)
+                .addTo(mapInstance);
 
-                setMarkers([...markers, newMarker]);
-            };
+            setMarkers([...markers, newMarker]);
+        };
 
-            const handleMapClick = (e) => {
-                const lng = e.lngLat.lng;
-                const lat = e.lngLat.lat;
-                createMarker([lng, lat], map);
-            }
+        const handleMapClick = (e) => {
+            const lng = e.lngLat.lng;
+            const lat = e.lngLat.lat;
+            createMarker([lng, lat], map);
+        }
 
-            map.on("click", handleMapClick);
+        map.on("click", handleMapClick);
 
 
         return () => map.remove();

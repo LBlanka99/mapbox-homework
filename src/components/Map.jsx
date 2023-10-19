ed52821647007f96190f5845df68b5a24a7b4fbb
LBlanka99 <losonci.blanka@mensa.hu>
2023-10-19 10:21:50 +0200

    Display route with fixed starting point

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -30,6 +30,92 @@ const Map = () => {
         map.addControl(new mapboxgl.GeolocateControl(), "bottom-right");
 
 
+        const start = [17.915123, 47.093123];
+
+        // create a function to make a directions request
+        async function getRoute(end) {
+            // make a directions request using cycling profile
+            // an arbitrary start will always be the same
+            // only the end or destination will change
+            const query = await fetch(
+                `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=false&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+                {method: 'GET'}
+            );
+            const json = await query.json();
+            const data = json.routes[0];
+            const route = data.geometry.coordinates;
+            const geojson = {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                    type: 'LineString',
+                    coordinates: route
+                }
+            };
+            // if the route already exists on the map, we'll reset it using setData
+            if (map.getSource('route')) {
+                map.getSource('route').setData(geojson);
+            }
+            // otherwise, we'll make a new request
+            else {
+                map.addLayer({
+                    id: 'route',
+                    type: 'line',
+                    source: {
+                        type: 'geojson',
+                        data: geojson
+                    },
+                    layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    paint: {
+                        'line-color': '#52358c',
+                        'line-width': 5,
+                        'line-opacity': 0.85
+                    }
+                });
+            }
+        }
+
+        map.on('load', () => {
+            // make an initial directions request that
+            // starts and ends at the same location
+            getRoute(start);
+
+            // Add starting point to the map
+            map.addLayer({
+                id: 'point',
+                type: 'circle',
+                source: {
+                    type: 'geojson',
+                    data: {
+                        type: 'FeatureCollection',
+                        features: [
+                            {
+                                type: 'Feature',
+                                properties: {},
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: start
+                                }
+                            }
+                        ]
+                    }
+                },
+                paint: {
+                    'circle-radius': 10,
+                    'circle-color': '#3887be'
+                }
+            });
+            map.on('click', (event) => {
+                const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+
+                getRoute(coords);
+            });
+        });
+
+
         const createMarker = (coordinates, mapInstance) => {
             //TODO check if there is already a marker at these coordinates
             const rotationDegree = Math.random() < 0.5 ? 10 : -10;
